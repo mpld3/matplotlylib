f627f579d1563b4e3a64a4bf1d25fbfde17eef29
theengineear <andseier@gmail.com>
2014-03-25 19:44:20 -0700

    Commit for test-PyPi registration.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# try:
+#     from setuptools import setup
+# except ImportError:
+from distutils.core import setup
 
 DESCRIPTION = "Matplotlib to Plotly Converter"
 LONG_DESCRIPTION = open('README.md').read()
@@ -12,7 +12,7 @@ MAINTAINER = "Andrew Seier"
 MAINTAINER_EMAIL = "andseier@gmail.com"
 URL = 'http:/mpld3.github.com'
 DOWNLOAD_URL = 'https://github.com/mpld3/matplotlylib'
-LICENSE = 'BSD 3-clause'
+LICENSE = 'MIT'
 VERSION = '0.1'
 
 setup(name=NAME,
